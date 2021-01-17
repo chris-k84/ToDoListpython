f1c51e5d5a02a05a32a0d6fa164b22dca3b99771
Chris Knight <ChrisK@beckhoff.com>
2021-01-17 20:37:47 +0000

    defined model class ToDoListItem, added constructor to ViewClass

---
+++ src/ToDoListModel.py
@@ -0,0 +1,4 @@
+class ToDoListItem():
+    def __init__(self):
+        self.taskname = ""
+        self.taskdescription = ""
---
+++ src/ToDoListView.py
@@ -0,0 +1,5 @@
+import tkinter as Tk
+
+class View():
+    def __init__(self, master):
+        self.frame = Tk.Frame(master)
