acd405f59e3a83e836cf58e37cf9fa2d9d87f88f
Chris Knight <ChrisK@beckhoff.com>
2021-01-19 16:53:58 +0000

    solved widgets not appearing bug, need to be careful with frame placement. added listbox for showing saved tasks

--- src/ToDoListView.py
+++ src/ToDoListView.py
@@ -3,18 +3,23 @@ import tkinter as Tk
 class View():
     def __init__(self, master):
         master.title("Python To Do List")
-        self.frame = Tk.Frame(master)
-        
-        tasknamelabel = Tk.Label(text= "Task Name")
-        descriptionlabel = Tk.Label(text= "Task Description")
+        listFrame = Tk.Frame(master)
+        inputFrame = Tk.Frame(master)
+        listFrame.grid(row=1,column=1)
+        inputFrame.grid(row=1, column=2)
+
+        tasknamelabel = Tk.Label(inputFrame,text= "Task Name")
+        descriptionlabel = Tk.Label(inputFrame, text= "Task Description")
         self.TaskName = Tk.StringVar()
         self.TaskDescription = Tk.StringVar()
-        taskentry = Tk.Entry(textvariable=self.TaskName)
-        descriptionentry = Tk.Entry(textvariable=self.TaskDescription)
+        taskentry = Tk.Entry(inputFrame,textvariable=self.TaskName)
+        descriptionentry = Tk.Entry(inputFrame,textvariable=self.TaskDescription)
         
         tasknamelabel.grid(row=1, column=1) 
         descriptionlabel.grid(row=2, column=1)
         taskentry.grid(row=1, column=2)
         descriptionentry.grid(row= 2, column=2)
+
+        taskListView = Tk.Listbox(listFrame)
         
 
