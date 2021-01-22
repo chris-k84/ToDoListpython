7aed7849e5cf8761c0c1996efbdbb3736c530cc4
Chris Knight <ChrisK@beckhoff.com>
2021-01-22 22:49:00 +0000

    added modelview command binds to buttons, need to update listbox with TaskList live

--- src/ToDoListModelView.py
+++ src/ToDoListModelView.py
@@ -1,12 +1,26 @@
 import tkinter as Tk
 import ToDoListView as Tdview
+import ToDoListModel as TdModel
 
 
 class ModelView():
     def __init__(self):
         self.root = Tk.Tk()
         self.view = Tdview.View(self.root)
+        self.view.AddButton.bind("<Button>",self.add)
+        #self.view.RemoveButton.bind("<Button>", self.remove)
+        self.TaskList = []
+        self.Task = TdModel.ToDoListItem
 
     def run(self):
         self.root.title("To Do List")
         self.root.mainloop()
+
+    def add(self, event):
+        self.Task.taskname = self.view.TaskName
+        self.Task.taskdescription = self.view.TaskDescription
+        self.TaskList.append(self.Task)
+
+    def remove(self, event):
+        self.TaskList.remove(self.view.taskListView.curselection)
+
