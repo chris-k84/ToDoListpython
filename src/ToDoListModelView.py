099591e6ff2dee3d022c3b56518994db8b9bec71
Chris Knight <ChrisK@beckhoff.com>
2021-01-23 23:17:28 +0000

    listbox updates on Add button press, but list is being overwritten passsing task object need to fix this

--- src/ToDoListModelView.py
+++ src/ToDoListModelView.py
@@ -8,19 +8,25 @@ class ModelView():
         self.root = Tk.Tk()
         self.view = Tdview.View(self.root)
         self.view.AddButton.bind("<Button>",self.add)
-        #self.view.RemoveButton.bind("<Button>", self.remove)
+        self.view.RemoveButton.bind("<Button>", self.remove)
         self.TaskList = []
-        self.Task = TdModel.ToDoListItem
+        self.view.taskListView.insert(Tk.END,self.TaskList)
 
     def run(self):
         self.root.title("To Do List")
         self.root.mainloop()
 
     def add(self, event):
-        self.Task.taskname = self.view.TaskName.get()
-        self.Task.taskdescription = self.view.TaskDescription.get()
-        self.TaskList.append(self.Task)
+        task = TdModel.ToDoListItem
+        task.taskname = self.view.TaskName.get()
+        task.taskdescription = self.view.TaskDescription.get()
+        #self.TaskList.append(task)
+        test = task.taskname + " " + task.taskdescription
+        self.TaskList.append(test)
+        self.view.taskListView.insert(Tk.END, "{:1}{:5}".format(task.taskname, task.taskdescription) )
 
     def remove(self, event):
-        self.TaskList.remove(self.view.taskListView.curselection)
+        index = self.view.taskListView.curselection()
+        self.TaskList.remove(index)
+        self.view.taskListView.delete(index)
 
