1ec99b0c86a4307a8f10c5ebf4b9ba9c41d09cea
Chris Knight <ChrisK@beckhoff.com>
2021-02-06 23:29:31 +0000

    added onClose and onStart event methods to save and load the task list to a json file, need to format the return info

--- src/ToDoListModelView.py
+++ src/ToDoListModelView.py
@@ -1,16 +1,18 @@
 import tkinter as Tk
 import ToDoListView as Tdview
 import ToDoListModel as TdModel
+import json
 
 
 class ModelView():
     def __init__(self):
         self.root = Tk.Tk()
+        self.root.protocol("WM_DELETE_WINDOW", self.onClose)
         self.view = Tdview.View(self.root)
         self.view.AddButton.bind("<Button>",self.add)
         self.view.RemoveButton.bind("<Button>", self.remove)
         self.TaskList = []
-        self.view.taskListView.insert(Tk.END,self.TaskList)
+        self.onStart()
 
     def run(self):
         self.root.title("To Do List")
@@ -20,7 +22,6 @@ class ModelView():
         task = TdModel.ToDoListItem
         task.taskname = self.view.TaskName.get()
         task.taskdescription = self.view.TaskDescription.get()
-        #self.TaskList.append(task)
         test = task.taskname + " " + task.taskdescription
         self.TaskList.append(test)
         self.view.taskListView.insert(Tk.ACTIVE, "{:<1s}{:>25s}".format(task.taskname, task.taskdescription) )
@@ -33,4 +34,16 @@ class ModelView():
         if (index[0]+1) != size:
             del self.TaskList[index[0]]
             self.view.taskListView.delete(index)
+
+    def onClose(self):
+        with open('test.json','w') as fh:
+            json.dump(self.TaskList,fh)
+        self.root.destroy()
+
+    def onStart(self):
+        with open('test.json') as fh:
+            self.TaskList = json.load(fh)
+        self.view.taskListView.insert(Tk.END,self.TaskList)
+        #for task in self.TaskList:
+
 
